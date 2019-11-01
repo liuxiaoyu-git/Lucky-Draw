b507926e6611e9334771736926537d3e707e7066
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 22:32:16 +0800

    Update createsampledate.php

--- createsampledate.php
+++ createsampledate.php
@@ -8,7 +8,7 @@ $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 
 for (int i=1;i<10;i++){
-  $sql = "insert into registryuser values('user".i".-0001')";
+  $sql = "insert into registryuser values('user".i."-0001')";
   if ($connection->query($sql) !== TRUE) {  
       echo "发生数据库操作错误";  
   } 

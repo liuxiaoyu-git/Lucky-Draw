990ff08d0dc0531034e11f8a625a6a88fda0820e
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-02 01:18:00 +0800

    Update test.php

--- test.php
+++ test.php
@@ -6,10 +6,35 @@ $dbname = getenv("DATABASE_NAME"); #sampledb
 $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 
-$sql = "select username from registryuser where username not in (select username from winner)";
-$rs = $connection->query($sql);
+//清空抽奖表
+$sql = "delete from TEMPLUCKYDRAW";
+echo $connection->query($sql);
 
-while($row = mysqli_fetch_assoc($rs))
-    echo $row['username']."<br/>"; 
+//将可以抽奖的人员复制到抽奖表
+$sql = "insert into TEMPLUCKYDRAW select (@rowno:= @rowno+1) AS rowno, username from registryuser, (SELECT @rowno:=0) as rowno where username not in (select username from winner)";
+echo $connection->query($sql);
+
+//获得可抽奖的总人数
+$sql = "select count(*) as count from TEMPLUCKYDRAW";
+$rs = $connection->query($sql);
+$row = mysqli_fetch_assoc($rs);
+//$num_rows = mysql_num_rows($rs);
+
+//抽奖，中奖号
+$winner_row=rand(1,$row['count']); 
+
+$sql = "select username from TEMPLUCKYDRAW where rowno='".$winner_row."';
+echo $sql."<br/>";
+$rs = $connection->query($sql);
+$row = mysqli_fetch_assoc($rs);
+echo $row['username']."<br/>"; 
+
+//$sql = "create table TEMPLUCKYDRAW as select (@rowno:= @rowno+1) AS rowno, username from registryuser, (SELECT @rowno:=0) as rowno where username not in (select username from winner)";
+//$sql = "drop table TEMPLUCKYDRAW";
+//$sql = "select (@rowno:= @rowno+1) AS rowno, username from registryuser, (SELECT @rowno:=0) as rowno where username not in (select username from winner) and rowno=".$num_winner;
+
+
+//while($row = mysqli_fetch_assoc($rs))
+//   echo $row['username']."<br/>"; 
 
 mysqli_close($connection);

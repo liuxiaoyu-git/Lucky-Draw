18a021074df8ee49bf472fd458f3688df7b0e125
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 21:15:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<a href="initapp.php">
-1）初始化抽奖应用（清除所有注册抽奖的用户）
-</a>
-<br/>
-<a href="luckydraw.php">
-2）去抽奖
-</a>
-<br/>
-<a href="showwinner.php">
-3）显示已中奖用户
-</a>
+<html>
+    <body>
+        <table width="100%" height="100%">
+            <tr>
+                <td align="center">
+                  <a href="luckydraw.php">
+                  1）去抽奖
+                  </a>
+                  <br/>
+                  <a href="showwinner.php">
+                  2）显示已中奖用户
+                  </a>
+                  <br/>
+                  <a href="initapp.php">
+                  3）重置抽奖应用（清除所有注册抽奖的用户）
+                  </a>
+                  <br/>
+                  <a href="sample.php">
+                  4）生成模拟数据
+                  </a>
+                  <br/>
+                  <a href="test.php">
+                  5）功能测试
+                  </a>
+				      </td>
+			    </tr>
+        </table>
+    </body>
+</html>

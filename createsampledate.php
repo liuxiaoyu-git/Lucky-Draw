1fffea254d70ede5fb0654d15b7df314b2ab5221
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 22:47:41 +0800

    Update createsampledate.php

--- createsampledate.php
+++ createsampledate.php
@@ -1,4 +1,3 @@
-
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
@@ -7,7 +6,7 @@ $dbname = getenv("DATABASE_NAME"); #sampledb
 $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 
-for (int $i=1; $i<10; $i++){
+for ($i=1; $i<=10; $i++){
   $sql = "insert into registryuser values('user".$i."-0001')";
   if ($connection->query($sql) !== TRUE) {  
       echo "发生数据库操作错误";  
@@ -18,7 +17,7 @@ $sql = "insert into winner values('user3-0003','special prize')";
 if ($connection->query($sql) !== TRUE) {  
     echo "发生数据库操作错误";  
 } 
-mysqli_close($connection);
 
+mysqli_close($connection);
 echo "测试数据加载完毕。";
 ?>

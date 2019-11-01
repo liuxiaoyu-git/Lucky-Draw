762de5c2d99ebe7940691598e7e84dd5dc84efd9
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 23:23:36 +0800

    Update test.php

--- test.php
+++ test.php
@@ -6,16 +6,16 @@ $dbname = getenv("DATABASE_NAME"); #sampledb
 $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 
-$sql='select username from registryuser where username not in (select username from winner)';
-$rs=$connection->query($sql);
-$result = array();
+$sql = "select username from registryuser where username not in (select username from winner)";
+$rs = $connection->query($sql);
+//$result = array();
 
-while($row = mysqli_fetch_array($rs)) 
-    echo $row;
-    $result[]=$row;
+while($row = mysqli_fetch_assoc($rs)) 
+    echo $row['username'];
+    //$result[]=$row;
 }
 mysqli_close($connection);
 
 echo '查询结果在下面的额为数组里面:<pre>';
-print_r($result);
+//print_r($result);
 echo '</pre>';

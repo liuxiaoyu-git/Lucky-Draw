e5ab65c22025ff07ff8ab1f53e209f95ab9498d2
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 23:43:29 +0800

    Update test.php

--- test.php
+++ test.php
@@ -7,13 +7,14 @@ $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 
 $sql = "select username from registryuser where username not in (select username from winner)";
-$rs = $connection->query($sql);
+$rs = mysql_query($sql);
 //$result = array();
 
 while($row = mysql_fetch_array($result)) {
     echo $row[0]."<br/>"; 
     //$result[]=$row;
 }
+mysql_free_result($rs);
 mysqli_close($connection);
 
 echo "<br/>查询结果在下面的额为数组里面:<pre>";

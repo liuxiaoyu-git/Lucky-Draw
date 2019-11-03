0e1525739f78deef5cd0b48a32f73d28a95cf9f1
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 18:38:34 +0800

    Update showwinner.php

--- showwinner.php
+++ showwinner.php
@@ -8,10 +8,9 @@ $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $sql = "select username,round from winner order by round";
 $rs = $connection->query($sql);
-echo "<table><tr><td>奖项</td><td>中奖人</td></tr>"
-while ($row = mysqli_fetch_assoc($rs)) 
+echo "<table><tr><td>奖项</td><td>中奖人</td></tr>";
+while($row = mysqli_fetch_assoc($rs))
 	echo "<tr><td>".$row['round']."</td><td>".$row['username']."</td></tr>";
-mysqli_close($connection);
-
 echo "</table>";                 
+mysqli_close($connection);
 ?>

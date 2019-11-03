628a6e8d56bdd12bb52c943697a0979f256bb5f9
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 16:42:08 +0800

    Update luckydraw.php

--- luckydraw.php
+++ luckydraw.php
@@ -7,8 +7,8 @@ $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $namelist = "";
 $sql = "select username from registryuser where username not in (select username from winner)";
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$rs=$connection->query($sql)；
-while($row=mysqli_fetch_assoc($rs)) 
+$rs=$connection->query($sql);
+while($row=mysqli_fetch_assoc($rs)) 
 	$namelist=$namelist."'".$row['username']."',";
 $namelist=substr($namelist,0,strlen($namelist)-1);
 

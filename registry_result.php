e3546af6e5ad70495344efb3bbc3ef7edea1271d
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 09:50:34 +0800

    Create registry_result.php

---
+++ registry_result.php
@@ -0,0 +1,19 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER"); #openshift
+$dbname = getenv("DATABASE_NAME"); #sampledb
+$dbpwd = getenv("DATABASE_PASSWORD"); #password
+$username = $_POST['username'];
+$code = $_POST['code'];
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+$sql = "insert into registryuser values(".$registryuser.",".$code.")";
+if ($connection->query($sql) !== TRUE) {  
+    echo "发生数据库操作错误或用户已经存在";  
+} 
+mysqli_close($connection);
+
+echo "抽奖用户注册成功。";
+echo "<br/>用户名：".$username;
+echo "<br/>手机后4位：".$code;
+?>

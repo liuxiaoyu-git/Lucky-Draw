0578271c9269173577e512dade18721248865f75
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 16:20:29 +0800

    Update registry.php

--- registry.php
+++ registry.php
@@ -2,9 +2,9 @@
 <head>
 <body>
 <form action=registry_result.php method=post>
-用户名：<input type="text" name="username" /><br/>
-手机后4位<input type="text" name="code" /><br/>
-<input type="summit" value="提交抽奖用户"/>
+用户姓名 ：<input type="text" name="username" /><br/>
+手机后4位：<input type="text" name="code" /><br/>
+<input type="submit" value="提交抽奖用户"/>
 </form>
 </body>
 </html>

d08419b022d42dec40814e362fc29d6cbbf9ce3a
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 10:19:31 +0800

    Update registry_result.php

--- registry_result.php
+++ registry_result.php
@@ -9,11 +9,11 @@ $code = $_POST['code'];
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $sql = "insert into registryuser values(".$registryuser.",".$code.")";
 if ($connection->query($sql) !== TRUE) {  
-    echo "发生数据库操作错误或用户已经存在";  
-} 
+  echo "发生数据库操作错误或用户".$username."已经存在";  
+} esle {
+	echo "抽奖用户注册成功。";
+	echo "<br/>用户名：".$username;
+	echo "<br/>手机后4位：".$code;
+}
 mysqli_close($connection);
-
-echo "抽奖用户注册成功。";
-echo "<br/>用户名：".$username;
-echo "<br/>手机后4位：".$code;
 ?>

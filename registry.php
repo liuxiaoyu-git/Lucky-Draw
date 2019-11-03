244d5c4cb8aee696cb5901835614e7105572977b
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-03 09:41:36 +0800

    Create registry.php

---
+++ registry.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<body>
+<form action=registry_result.php method=post>
+用户名：<input type="text" name="username" /><br/>
+手机后4位<input type="text" name="code" /><br/>
+<input type="summit" value="提交抽奖用户"/>
+</form>
+</body>
+</html>
